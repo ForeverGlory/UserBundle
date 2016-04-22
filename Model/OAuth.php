9c0214be2c6827080047dc6c41562db2d7d7f004
ForeverGlory <foreverglory@qq.com>
2016-04-23 02:54:04 +0800

    add model

---
+++ Model/OAuth.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * (c) ForeverGlory <http://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ */
+
+namespace Glory\Bundle\UserBundle\Model;
+
+/**
+ * Description of OAuth
+ *
+ * @author ForeverGlory <foreverglory@qq.com>
+ */
+class OAuth
+{
+    
+}
---
+++ Model/User.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * (c) ForeverGlory <http://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ */
+
+namespace Glory\Bundle\UserBundle\Model;
+
+use FOS\UserBundle\Model\User as FOSUser;
+
+/**
+ * Description of User
+ *
+ * @author ForeverGlory <foreverglory@qq.com>
+ */
+class User extends FOSUser
+{
+    
+}
