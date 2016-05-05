380b4795d225f8133f0a1cfb1769f93e60740574
ForeverGlory <foreverglory@qq.com>
2016-05-06 00:42:32 +0800

    admin/group

---
+++ Controller/Admin/GroupController.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * (c) ForeverGlory <http://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ */
+
+namespace Glory\Bundle\UserBundle\Controller\Admin;
+
+use FOS\UserBundle\Controller\GroupController as BaseGroupController;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Description of GroupController
+ *
+ * @author ForeverGlory <foreverglory@qq.com>
+ */
+class GroupController extends BaseGroupController
+{
+
+    public function createAction(Request $request)
+    {
+        
+    }
+
+}
