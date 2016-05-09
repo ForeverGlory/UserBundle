6400d3ed8118c23b8e841bc9d444d9c2d6e27684
ForeverGlory <foreverglory@qq.com>
2016-05-09 23:21:18 +0800

    user/group

--- Controller/Admin/GroupController.php
+++ Controller/Admin/GroupController.php
@@ -19,9 +19,29 @@ use Symfony\Component\HttpFoundation\Request;
 class GroupController extends BaseGroupController
 {
 
+    public function listAction()
+    {
+        return parent::listAction();
+    }
+
     public function createAction(Request $request)
     {
-        
+        return parent::newAction($request);
+    }
+
+    public function editAction(Request $request, $groupName)
+    {
+        return parent::editAction($request, $groupName);
+    }
+
+    public function deleteAction(Request $request, $groupName)
+    {
+        return parent::deleteAction($request, $groupName);
+    }
+
+    public function showAction($groupName)
+    {
+        return parent::showAction($groupName);
     }
 
 }
