dab389898c19cbccdacc2d9671b3d29ce76c9df5
ForeverGlory <foreverglory@qq.com>
2016-05-16 17:21:54 +0800

    格式化代码，并增加 findUsers($ariteria,$orderBy,$limit,$offset)

--- Model/UserManager.php
+++ Model/UserManager.php
@@ -1,21 +1,23 @@
 <?php
 
 /*
+ * This file is part of the current project.
+ * 
  * (c) ForeverGlory <http://foreverglory.me/>
  * 
  * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Glory\Bundle\UserBundle\Model;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
-use Glory\Bundle\UserBundle\Model\User;
-use Glory\Bundle\UserBundle\Model\OAuth;
 use Doctrine\Bundle\DoctrineBundle\Registry;
 
 /**
- * Description of UserManager
+ * UserManager
+ * 
+ * extends 'fos_user.user_manager' service
  *
  * @author ForeverGlory <foreverglory@qq.com>
  */
@@ -23,114 +25,52 @@ class UserManager implements UserManagerInterface
 {
 
     protected $container;
+    protected $class;
 
     public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
     }
 
-    protected function getUserClass()
-    {
-        return 'Glory\\Bundle\\UserBundle\\Entity\\User';
-    }
-
-    protected function getOAuthClass()
-    {
-        return 'Glory\\Bundle\\UserBundle\\Entity\\OAuth';
-    }
-
-    public function findOAuth($criteria = array())
-    {
-        $repository = $this->getDoctrine()->getRepository($this->getOAuthClass());
-        return $repository->findOneBy($criteria);
-    }
-
-    public function findOAuthByUser(User $user)
-    {
-        return $this->findOAuth(array('user' => $user));
-    }
-
     /**
+     * 'fos_user.user_manager' -> $method
      * 
-     * @param UserResponseInterface $response
-     * @return OAuth
+     * @param type $method
+     * @param type $arguments
+     * @return
      */
-    public function createOAuthFromResponse(UserResponseInterface $response)
-    {
-        $oauthClass = $this->getOAuthClass();
-        $oauth = new $oauthClass();
-        $oauth->setOwner($response->getResourceOwner()->getName());
-        $oauth->setUsername($response->getUsername());
-        $oauth->setCreated();
-        $this->updateOAuthFromResponse($oauth, $response, true);
-        return $oauth;
-    }
-
-    /**
-     * 
-     * @param OAuth $oauth
-     * @param UserResponseInterface $response
-     * @return OAuth
-     */
-    public function updateOAuthFromResponse(OAuth $oauth, UserResponseInterface $response, $andFlush = false)
-    {
-        $oauth->setNickname($response->getNickname());
-        $oauth->setFirstname($response->getFirstName());
-        $oauth->setLastname($response->getLastName());
-        $oauth->setRealname($response->getRealName());
-        $oauth->setEmail($response->getEmail());
-        $oauth->setAvatar($response->getProfilePicture());
-        $oauth->setAccesstoken($response->getAccessToken());
-        $oauth->setRefreshtoken($response->getRefreshToken());
-        $oauth->setTokensecret($response->getTokenSecret());
-        $oauth->setExpires($response->getExpiresIn());
-        $this->updateOAuth($oauth, $andFlush);
-    }
-
-    public function updateOAuth(OAuth $oauth, $andFlush = true)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($oauth);
-        if ($andFlush) {
-            $em->flush();
-        }
-    }
-
-    public function bindOAuth(User $user, OAuth $oauth)
-    {
-        $oauth->setUser($user);
-        $this->updateOAuth($oauth, true);
-        $user->addOAuth($oauth);
-    }
-
-    public function unBindOAuth(User $user, OAuth $oauth)
-    {
-        $oauth->setUser(null);
-        $this->updateOAuth($oauth, true);
-    }
-
-    /**
-     * Shortcut to return the Doctrine Registry service.
-     *
-     * @return Registry
-     *
-     * @throws \LogicException If DoctrineBundle is not available
-     */
-    public function getDoctrine()
-    {
-        if (!$this->container->has('doctrine')) {
-            throw new \LogicException('The DoctrineBundle is not registered in your application.');
-        }
-
-        return $this->container->get('doctrine');
-    }
-
-    public function __call($name, $arguments)
+    public function __call($method, $arguments)
     {
         $userManager = $this->getFOSUserManager();
-        if (method_exists($userManager, $name)) {
-            return call_user_func_array(array($userManager, $name), $arguments);
+        if (method_exists($userManager, $method)) {
+            return call_user_func_array(array($userManager, $method), $arguments);
         }
+        throw new \LogicException(sprintf('%s method is not exists', $method));
+    }
+
+    public function setClass($class)
+    {
+        $this->class = $class;
+        return $this;
+    }
+
+    public function getClass()
+    {
+        return $this->class? : $this->getFOSUserManager()->getClass();
+    }
+
+    /**
+     * 查找用户
+     * @param array $criteria
+     * @param array $orderBy
+     * @param type $limit
+     * @param type $offset
+     * @return Traversable
+     */
+    public function findUsers(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+    {
+        $repository = $this->getDoctrine()->getRepository($this->getClass());
+        return $repository->findBy($criteria, $orderBy, $limit, $offset);
     }
 
     /**
@@ -141,16 +81,19 @@ class UserManager implements UserManagerInterface
         return $this->container->get('fos_user.user_manager');
     }
 
-    public function findUsers(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+    /**
+     * Shortcut to return the Doctrine Registry service.
+     *
+     * @return Registry
+     *
+     * @throws \LogicException If DoctrineBundle is not available
+     */
+    protected function getDoctrine()
     {
-
-        $userManager = $this->getFOSUserManager();
-        return $userManager->findUsers();
-    }
-
-    public function getClass()
-    {
-        return $this->getFOSUserManager()->getClass();
+        if (!$this->container->has('doctrine')) {
+            throw new \LogicException('The DoctrineBundle is not registered in your application.');
+        }
+        return $this->container->get('doctrine');
     }
 
 }
