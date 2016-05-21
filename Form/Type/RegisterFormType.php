c20a180b48a1c4c8e9f9bef627744e06ede75d1d
ForeverGlory <foreverglory@qq.com>
2016-05-21 23:37:22 +0800

    register form type

---
+++ Form/Type/RegisterFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the current project.
+ * 
+ * (c) ForeverGlory <https://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Glory\Bundle\UserBundle\Form\Type;
+
+use FOS\UserBundle\Form\Type\RegistrationFormType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * Description of RegisterFormType
+ *
+ * @author ForeverGlory <foreverglory@qq.com>
+ */
+class RegisterFormType extends RegistrationFormType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+        $builder->remove('email');
+    }
+    public function getName()
+    {
+        return 'glory_user_register';
+    }
+}
