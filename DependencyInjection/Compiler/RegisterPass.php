a27e4ba50ce9ae84894bd79b41d81a8a3f5984eb
ForeverGlory <foreverglory@qq.com>
2016-05-21 23:37:57 +0800

    register pass, 更改默认的register form type

---
+++ DependencyInjection/Compiler/RegisterPass.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the current project.
+ * 
+ * (c) ForeverGlory <https://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Glory\Bundle\UserBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+
+/**
+ * check register
+ *
+ * @author ForeverGlory <foreverglory@qq.com>
+ */
+class RegisterPass implements CompilerPassInterface
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $formType = 'fos_user.registration.form.type';
+        if ($container->hasParameter($formType)) {
+            $value = $container->getParameter($formType);
+            if ($value == 'fos_user_registration') {
+                $container->setParameter($formType, 'glory_user_register');
+            }
+        }
+
+        $formName = 'fos_user.registration.form.name';
+        if ($container->hasParameter($formName)) {
+            $value = $container->getParameter($formName);
+            if ($value == 'fos_user_registration_form') {
+                $container->setParameter($formName, 'glory_user_register_form');
+            }
+        }
+    }
+
+}
--- GloryUserBundle.php
+++ GloryUserBundle.php
@@ -1,14 +1,32 @@
 <?php
 
+/*
+ * This file is part of the current project.
+ * 
+ * (c) ForeverGlory <https://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Glory\Bundle\UserBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Glory\Bundle\UserBundle\DependencyInjection\Compiler\RegisterPass;
 
+/**
+ * build
+ *
+ * @author ForeverGlory <foreverglory@qq.com>
+ */
 class GloryUserBundle extends Bundle
 {
+
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
+        $container->addCompilerPass(new RegisterPass());
     }
+
 }
