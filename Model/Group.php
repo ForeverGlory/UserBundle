c89722e116b2ec8e98639301ec5dde87ae7c0a71
ForeverGlory <foreverglory@qq.com>
2016-05-22 19:59:33 +0800

    用户组表单增加验证

--- Model/Group.php
+++ Model/Group.php
@@ -1,23 +1,33 @@
 <?php
 
 /*
- * (c) ForeverGlory <http://foreverglory.me/>
+ * This file is part of the current project.
+ * 
+ * (c) ForeverGlory <https://foreverglory.me/>
  * 
  * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Glory\Bundle\UserBundle\Model;
 
 use FOS\UserBundle\Model\Group as FOSGroup;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * Description of Group
- *
+ * 
+ * @UniqueEntity("name")
  * @author ForeverGlory <foreverglory@qq.com>
  */
 class Group extends FOSGroup
 {
 
+    /**
+     * @Assert\NotBlank()
+     */
+    protected $name;
     protected $createdTime;
     protected $updateTime;
 
